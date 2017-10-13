f67bee64b9d7a1d570014e3032af27e88dbc18ee
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-13 20:25:04 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,5 +17,5 @@ function divSelector(){
 
 function firstListItem(){
   //return $('img[src="https://s3.amazonaws.com/after-school-assets/awesome.jpg"]')
-  return $('ul img:first-child')[0];
+  return $('ul img:first-child').first();
 }
